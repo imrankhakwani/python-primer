a9230649791496181ae3fb9b68f2717a6e786fed
imrankhakwani <30284761+imrankhakwani@users.noreply.github.com>
2023-07-23 18:34:28 +0500

    Update tkinter_win.py

--- src/tkinter_win.py
+++ src/tkinter_win.py
@@ -1,5 +1,36 @@
-
 import tkinter as tk
-from tkinter import *
+from tkinter import ttk
 
-window_ = Tk()
+
+def main():
+    root_window = tk.Tk()
+    root_window.title = "Input Form"
+
+    base_frame = ttk.Frame(root_window)
+    base_frame.pack()
+
+    input_label = tk.Label(
+        base_frame,
+        text="Hello, Tkinter",
+        foreground="white",  # Set the text color to white
+        background="black"  # Set the background color to black
+    )
+    input_label.pack()
+
+    # standard_button = ttk.Button(base_frame)
+    # standard_button["text"] = 'Get input'
+    # standard_button["command"] = (lambda: get_input_value())
+    # standard_button.pack()
+
+    root_window.mainloop()
+
+
+def get_input_value():
+    print("You clicked button")
+
+
+# ----------------------
+# Calls  main  function.
+# ----------------------
+main()
+
